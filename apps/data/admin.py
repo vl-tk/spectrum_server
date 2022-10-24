8d1ab9e3323c4f1f5bae4f07b5ae55c4cd3320a8
vl-tk <vlad.work.39@ya.ru>
2022-10-24 17:00:57 +0200

    SimpleHistoryAdmin in CHZRecordAdmin

--- apps/data/admin.py
+++ apps/data/admin.py
@@ -8,7 +8,7 @@ from simple_history.admin import SimpleHistoryAdmin
 
 
 @admin.register(CHZRecord)
-class CHZRecordAdmin(CursorPaginatorAdmin):
+class CHZRecordAdmin(SimpleHistoryAdmin, CursorPaginatorAdmin):
 
     show_full_result_count = False
 
