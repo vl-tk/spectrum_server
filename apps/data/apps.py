8af5802e78b5ce65a86277ac3ad1cdc3bb915fc8
vl-tk <vlad.work.39@ya.ru>
2022-10-24 17:56:40 +0200

    name

--- apps/data/apps.py
+++ apps/data/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class DataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.data'
+    verbose_name = 'Данные'
 
     def ready(self):
         import apps.data.signals
