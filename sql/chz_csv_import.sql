3f499a3a333efe19d4af58485398ed87dfd004c0
vl-tk <vlad.work.39@ya.ru>
2022-11-10 15:58:04 +0200

    sql import

---
+++ sql/chz_csv_import.sql
@@ -0,0 +1,8 @@
+-- COPY data_chzrecord(date,inn,owner_name,prid,gt,pg_name,product_name,producer_name,mrp,in_russia,out_total,out_whosale,out_retail,out_recycle)
+-- FROM /home/file-7f95eb97-1302-420f-bc38-d35d7e0cdbbd.csv
+-- DELIMITER ,
+-- CSV HEADER;
+
+-- CREATE INDEX inn_idx ON data_chzrecord(inn);
+-- CREATE INDEX product_name_idx ON data_chzrecord(product_name);
+-- CREATE INDEX owner_name_idx ON data_chzrecord(owner_name);
