5ba8a77e94a6fe388020ff92136ac0e1b1073b51
vl-tk <vlad.work.39@ya.ru>
2022-12-15 16:33:38 +0200

    reports tests

--- apps/report/tests.py
+++ apps/report/tests.py
@@ -1,8 +1,9 @@
 import pytest
-from apps.events.models import Event
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+
+from apps.events.models import Event
 from utils.test import get_test_document_files, get_test_excel_file
 
 
@@ -11,13 +12,43 @@ def test_report_events(authorized_client, imported_events, test_file_remove):
 
     assert Event.objects.count() == 42
 
-    # 2. test retrieval
+    resp = authorized_client.get(
+        reverse('events:event_report'),
+        {
+            'type': 'avg_per_month',
+            'value_field': 'bjudzhet',
+            'date_field': 'data_nachala',
+        }
+    )
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.json() == '{"(1, 2022)":151.6129032258,"(2, 2022)":100.0}'
 
     resp = authorized_client.get(
         reverse('events:event_report'),
         {
-            'type': 'budget_per_month'
+            'type': 'total_per_month',
+            'value_field': 'bjudzhet',
+            'date_field': 'data_nachala',
         }
     )
     assert resp.status_code == status.HTTP_200_OK
-    assert len(resp.data['results']) == 20  # default page size
+
+    resp = authorized_client.get(
+        reverse('events:event_report'),
+        {
+            'type': 'avg_per_day',
+            'value_field': 'bjudzhet',
+            'date_field': 'data_nachala',
+        }
+    )
+    assert resp.status_code == status.HTTP_200_OK
+
+    resp = authorized_client.get(
+        reverse('events:event_report'),
+        {
+            'type': 'total_per_day',
+            'value_field': 'bjudzhet',
+            'date_field': 'data_nachala',
+        }
+    )
+    assert resp.status_code == status.HTTP_200_OK
