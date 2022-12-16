3990a793244fb3e3bc8e0312475355ccb5db8a89
vl-tk <vlad.work.39@ya.ru>
2022-12-16 16:25:37 +0200

    HistoricalRecords for events

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,16 +1,17 @@
 import eav
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from simple_history.models import HistoricalRecords
 
 
 class Event(models.Model):
-
-    # TODO: historical records - with changes by whom
 
     sort = models.IntegerField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    history = HistoricalRecords()
 
     class Meta:
 
