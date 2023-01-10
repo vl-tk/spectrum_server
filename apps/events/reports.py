78d389f8348860a1111cf2ecf7e7afef35d14e1a
vl-tk <vlad.work.39@ya.ru>
2023-01-10 16:16:57 +0200

    reports fix

--- apps/events/reports.py
+++ apps/events/reports.py
@@ -2,6 +2,12 @@ import pandas as pd
 from apps.report.services import ReportBuilder
 
 
+def convert_to_date(key: tuple) -> str:
+    if len(key) == 2:
+        return f"{key[0]:0>2}.{key[1]}"
+    if len(key) == 3:
+        return f"{key[0]:0>2}.{key[1]:0>2}.{key[2]}"
+
 class EventReportBuilder(ReportBuilder):
 
     def __init__(self, data):
@@ -20,54 +26,49 @@ class EventReportBuilder(ReportBuilder):
 
         self.df = pd.DataFrame.from_records(self.rows)
 
+    def _prepare_index(self, date_field, value_field):
+        self.df.index = pd.to_datetime(self.df[date_field])  # format
+        self.df[value_field] = self.df[value_field].astype(float)
+
     def report(self, groupby, value_field):
         try:
             result = self.df.groupby(by=groupby)[value_field].mean()
         except KeyError as e:
             print(self.df.columns)
             ilogger.exception(e)
-            return
-        return result.to_json()
+        res = result.to_dict()
+
+        # format values
+
+        res_with_dates = {}
+        for k, v in res.items():
+            res_with_dates[convert_to_date(k)] = f'{v:.2f}'
+
+        return res_with_dates
 
     def report_avg_per_day(self, value_field, date_field):
-
-        self.df.index = pd.to_datetime(self.df[date_field])  # format
-
-        self.df[value_field] = self.df[value_field].astype(float)
-
+        self._prepare_index(date_field, value_field)
         return self.report(
             groupby=[self.df.index.day, self.df.index.month, self.df.index.year],
             value_field=value_field
         )
 
     def report_avg_per_month(self, value_field, date_field):
-
-        self.df.index = pd.to_datetime(self.df[date_field])  # format
-
-        self.df[value_field] = self.df[value_field].astype(float)
-
+        self._prepare_index(date_field, value_field)
         return self.report(
             groupby=[self.df.index.month, self.df.index.year],
             value_field=value_field
         )
 
     def report_sum_per_month(self, value_field, date_field):
-
-        self.df.index = pd.to_datetime(self.df[date_field])  # format
-
-        self.df[value_field] = self.df[value_field].astype(float)
-
+        self._prepare_index(date_field, value_field)
         return self.report(
             groupby=[self.df.index.month, self.df.index.year],
             value_field=value_field
         )
 
     def report_sum_per_day(self, value_field, date_field):
-
-        self.df.index = pd.to_datetime(self.df[date_field])  # format
-
-        self.df[value_field] = self.df[value_field].astype(float)
-
+        self._prepare_index(date_field, value_field)
         return self.report(
             groupby=[self.df.index.day, self.df.index.month, self.df.index.year],
             value_field=value_field
