9fd115e4cc71905bc79cd2b88ae471c0d68fa296
vl-tk <vlad.work.39@ya.ru>
2023-01-17 16:51:52 +0200

    dgisrecords_potential fix

--- apps/data/urls.py
+++ apps/data/urls.py
@@ -1,5 +1,5 @@
-from apps.data.views import CHZListView, DGisRecordListView
-# from apps.users.views import ClientListView, ClientRetrieveView
+from apps.data.views import (CHZListView, DGisRecordListView,
+                             DGisRecordPotentialListView)
 from django.urls import path
 from rest_framework import routers
 
@@ -12,6 +12,7 @@ urlpatterns = [
     # path('<int:pk>', ClientRetrieveView.as_view(), name='retrieve_client'),
     path('chzrecords', CHZListView.as_view(), name='chz_records'),
     path('dgisrecords', DGisRecordListView.as_view(), name='dgis_records'),
+    path('dgisrecords_potential', DGisRecordPotentialListView.as_view(), name='dgisrecords_potential'),
 ]
 
 urlpatterns += router.urls
--- apps/data/views.py
+++ apps/data/views.py
@@ -4,8 +4,12 @@ from typing import *
 
 from apps.data.models import CHZRecord, DGisRecord
 from apps.data.serializers import CHZRecordSerializer, DGisRecordSerializer
+from apps.events.models import Event
+from django.db import connection
+from django.db.models import ExpressionWrapper, F
 from django_filters.rest_framework import FilterSet
 from django_filters.rest_framework.filters import CharFilter
+from eav.models import Attribute, Value
 from main.pagination import StandardResultsSetPagination
 from rest_framework import generics, permissions, status
 from rest_framework.generics import ListAPIView
@@ -47,3 +51,48 @@ class DGisRecordListView(ListAPIView):
     queryset = DGisRecord.objects.all()
     pagination_class = StandardResultsSetPagination
     filterset_class = DGisRecordFilterSet
+
+
+class DGisRecordFilterSet(FilterSet):
+
+    class Meta:
+        model = DGisRecord
+        fields = ['city']
+        exclude = ['inn']
+
+
+class DGisRecordPotentialListView(ListAPIView):
+
+    permission_classes = [AllowAny]
+    serializer_class = DGisRecordSerializer
+    pagination_class = StandardResultsSetPagination
+    filterset_class = DGisRecordFilterSet
+
+    def get_queryset(self, *args, **kwargs):
+
+        # TODO: 'adresid_tochki' is hardcoded for now
+        a = Attribute.objects.get(slug='adresid_tochki')
+
+        cursor = connection.cursor()
+
+        sql = """
+            SELECT DISTINCT ev.entity_id AS id
+            FROM eav_value AS ev
+            INNER JOIN events_event AS et ON et.id = ev.entity_id
+            WHERE ev.value_text ~ '^\d+(\.\d+)?$' AND ev.attribute_id = {attribute_id}
+        """.format(
+            attribute_id=a.pk
+        )
+
+        try:
+            cursor.execute(sql)
+            ids_tuples = cursor.fetchall()
+        except Exception as e:
+            cursor.close
+            raise e
+
+        event_ids = [str(v[0]) for v in ids_tuples]
+
+        dgisrecord_ids = [e.eav.adresid_tochki for e in Event.objects.filter(id__in=event_ids)]
+
+        return DGisRecord.objects.exclude(id__in=dgisrecord_ids)
